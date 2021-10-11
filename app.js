4b71bb14811b9b040bf739cf4bb88ed3ad2efb0d
wooljam <woalskdl@gmail.com>
2021-10-11 22:24:49 +0900

    post, get routing까지

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
 app.listen(3000, function (){
-    console.log("start! express server on port 3000");
+    console.log("start!!! express server on port 3000");
 });
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+// url routing
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get('/main', function(req, res) {
+    res.sendFile(__dirname + "/public/main.html");
+});
+
+app.post('/email_post', function (req, res){
+    console.log(req.body.email);
+    // res.send("<h1>welcome! <br/> " + req.body.email + "</h1>");
+    res.render('email.ejs', {'email' : req.body.email})
+});
+
+app.post('/ajax_send_email', function (req, res){
+    console.log('ajax started');
+    // check validation > input value (select db)
+    let resData = {'result' : 'ok', 'email' : req.body.email};
+    res.json(resData);
+})
---
+++ public/main.js
@@ -0,0 +1,1 @@
+console.log("main.js has been loaded")
